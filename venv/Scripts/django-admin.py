8835a6a86d3182f6c5c609303a0ff31d23210648
Vadim <bardiervadim97@gmail.com>
2020-06-25 13:32:41 +0300

    'Initia_commit'

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Вадим\PycharmProjects\photo_social_network\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
